7b5b24333c441cddc9e28cb36248dca1e9cf0e9f
dreamiixeth <dreamiixeth@gmail.com>
2026-01-24 15:41:31 +0100

    Remove import of AnimatedSection component

--- agra-doo-clean/components/NewEquipmentCatalog.js
+++ agra-doo-clean/components/NewEquipmentCatalog.js
@@ -2,7 +2,6 @@
 
 import { useState, useEffect } from 'react'
 import { supabase } from '@/lib/supabase'
-import AnimatedSection from './AnimatedSection'
 
 // Definicija skupin za Kosilnice
 const KOSILNICE_GROUPS = [
