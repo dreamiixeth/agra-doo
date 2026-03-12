fd82c2938769319b22898a8b36593c0b3cea575f
dreamiixeth <dreamiixeth@gmail.com>
2026-03-12 10:38:44 +0100

    Update page.js

--- agra-doo-clean/app/page.js
+++ agra-doo-clean/app/page.js
@@ -161,7 +161,7 @@ function HeroSection() {
       </div>
 
       {/* Bottom fade */}
-      <div className="absolute bottom-0 left-0 right-0 h-32 bg-gradient-to-t from-[#F8F9FA] to-transparent" />
+      <div className="absolute bottom-0 left-0 right-0 h-32 bg-gradient-to-t from-[#DDE1E6] to-transparent" />
     </section>
   )
 }
